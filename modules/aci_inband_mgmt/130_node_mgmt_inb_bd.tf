2e1f0217d8f44d3eb879fcfcc6a80f7c8316a831
tiagosil-cisco <tiagosil@cisco.com>
2023-11-09 20:37:45 -0500

    small updates

--- modules/aci_inband_mgmt/130_node_mgmt_inb_bd.tf
+++ modules/aci_inband_mgmt/130_node_mgmt_inb_bd.tf
@@ -26,20 +26,20 @@ resource "aci_rest_managed" "node_mgmt_inb_contract_cons" {
   }
 }
 */
-resource "aci_rest" "node_mgmt_inb_contract_prov" {
-  depends_on = [aci_node_mgmt_epg.in_band]
-  path       = "/api/node/mo/uni/tn-mgmt/mgmtp-default/inb-${local.inband_epg_name}.json"
+resource "aci_rest_managed" "node_mgmt_inb_contract_prov" {
+  depends_on = [ aci_node_mgmt_epg.in_band ]
+  dn         = "uni/tn-mgmt/mgmtp-default/inb-${local.inband_epg_name}/rsprov-${aci_contract.INBAND_CONTRACT.name}"
   class_name = "fvRsProv"
   content = {
     tnVzBrCPName = aci_contract.INBAND_CONTRACT.name
   }
-}
+} 
 
-resource "aci_rest" "node_mgmt_inb_contract_cons" {
-  depends_on = [aci_node_mgmt_epg.in_band]
-  path       = "/api/node/mo/uni/tn-mgmt/mgmtp-default/inb-${local.inband_epg_name}.json"
+resource "aci_rest_managed" "node_mgmt_inb_contract_cons" {
+  depends_on = [ aci_node_mgmt_epg.in_band ]
+  dn         = "uni/tn-mgmt/mgmtp-default/inb-${local.inband_epg_name}/rscons-${aci_contract.INBAND_CONTRACT.name}"
   class_name = "fvRsCons"
   content = {
     tnVzBrCPName = aci_contract.INBAND_CONTRACT.name
   }
-}
+} 
